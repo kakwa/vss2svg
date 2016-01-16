15b9345a7c5a3ccf99448ee2fd239a591be8fa5c
kakwa <carpentier.pf@gmail.com>
2016-01-16 02:30:55 +0100

    adding test files

--- tests/resources/check_correctness.sh
+++ tests/resources/check_correctness.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 OUTDIR="../out"
-EMFDIR="./emf"
+vssDIR="./vss"
 ret=0
 VAGRIND_CMD="valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes --error-exitcode=42"
 
@@ -11,14 +11,14 @@ STOPONERROR="no"
 
 help(){
     cat <<EOF
-usage: `basename $0` [-h] [-v] [-e <emf dir>] [-s] [-n] 
+usage: `basename $0` [-h] [-v] [-e <vss dir>] [-s] [-n] 
 
 Script checking memleaks, segfault and svg correctness of vss2svg-conv
 
 arguments:
   -h: diplays this help
-  -v: verbose mode, print emf records
-  -e: alternate emf dir (default '`readlink -f "$ABSPATH/$EMFDIR"`')
+  -v: verbose mode, print vss records
+  -e: alternate vss dir (default '`readlink -f "$ABSPATH/$vssDIR"`')
   -s: stop on first error
   -r: resize to 800x600
   -x: disable xmllint check (svg integrity)
@@ -43,7 +43,7 @@ while getopts ":hnNxrvse:" opt; do
         VERBOSE_OPT='--verbose'
         ;;
     e)
-        EMFDIR=`readlink -f "$OPTARG" |sed "s%$ABSPATH%.%"`
+        vssDIR=`readlink -f "$OPTARG" |sed "s%$ABSPATH%.%"`
         ;;
     s)
         STOPONERROR="yes"
@@ -85,35 +85,39 @@ mkdir -p $OUTDIR
 CMD="`readlink -f ../../vss2svg-conv`"
 OUTDIR=`readlink -f $OUTDIR`
 DTD=`readlink -f ./svg11-flat.dtd`
-for emf in `find $EMFDIR -type f -name "*.emf" |sort`
+for vss in `find $vssDIR -type f -name "*.vss" |sort`
 do
-    EMF="`readlink -f $emf`"
-    SVG="${OUTDIR}/`basename ${emf}`.svg"
-    verbose_print "\n############## `basename "${emf}"` ####################"
-    verbose_print "Command: $CMD $RESIZE_OPTS -p -i \"$EMF\" -o \"${SVG}\""
-    $VAGRIND_CMD $CMD -p $RESIZE_OPTS -i "$EMF" -o ${SVG} $VERBOSE_OPT
+    vss="`readlink -f $vss`"
+    SVG="${OUTDIR}/`basename ${vss}`"
+    verbose_print "\n############## `basename "${vss}"` ####################"
+    verbose_print "Command: $CMD $RESIZE_OPTS -i \"$vss\" -o \"${SVG}\""
+    $VAGRIND_CMD $CMD $RESIZE_OPTS -i "$vss" -o ${SVG} $VERBOSE_OPT
     tmpret=$?
     if [ $tmpret -ne 0 ]
     then
-        printf "[${BYel}ERROR${RCol}] vss2svg-conv exited on error or memleaked or crashed converting emf '$EMF'\n"
+        printf "[${BYel}ERROR${RCol}] vss2svg-conv exited on error or memleaked or crashed converting vss '$vss'\n"
         [ $tmpret -eq 42 ] || [ $tmpret -eq 139 ] || ! [ "$IGNORECONVERR" = "yes" ] && ret=1
     fi
     if ! [ "$XMLLINT" = "no" ]
     then
-        xmllint --dtdvalid ./svg11-flat.dtd  --noout ${SVG} >/dev/null 2>&1
-        if [ $? -ne 0 ]
-        then
-            printf "[${BYel}ERROR${RCol}] vss2svg-conv generate bad svg\n"
-            printf "source emf:  $EMF\n"
-            printf "out svg   :  $SVG\n\n"
-            printf "xmllint result:\n"
-            xmllint --dtdvalid ./svg11-flat.dtd  --noout ${SVG} 2>&1 >/dev/null
-            printf "\n"
-            printf "Convert and xmllint commands:\n"
-            printf "$CMD -p -i \"$EMF\" -o \"${SVG}\"\n"
-            printf "xmllint --dtdvalid $DTD --noout ${SVG}\n\n"
-            ret=1
-        fi
+        for f in `ls ${SVG}/*`;
+        do
+            o=`readlink -f $f`
+            xmllint --dtdvalid ./svg11-flat.dtd  --noout $o >/dev/null 2>&1
+            if [ $? -ne 0 ]
+            then
+                printf "[${BYel}ERROR${RCol}] vss2svg-conv generate bad svg\n"
+                printf "source vss:  $vss\n"
+                printf "out svg   :  $SVG\n\n"
+                printf "xmllint result:\n"
+                xmllint --dtdvalid ./svg11-flat.dtd  --noout $o 2>&1 >/dev/null
+                printf "\n"
+                printf "Convert and xmllint commands:\n"
+                printf "$CMD -i \"$vss\" -o \"${SVG}\"\n"
+                printf "xmllint --dtdvalid $DTD --noout $o\n\n"
+                ret=1
+            fi
+        done
     fi
     verbose_print "\n#####################################################\n"
     [ "${STOPONERROR}" = "yes" ] && [ $ret -eq 1 ] && exit 1
--- tests/resources/check_corrupted.sh
+++ tests/resources/check_corrupted.sh
@@ -7,21 +7,21 @@ cd ..
 
 mkdir -p ../out
 
-# Once a file is seen as corrupted by emf2svg-conv
+# Once a file is seen as corrupted by vss2svg-conv
 # we alter if $MAX more times to try to make
-# emf2svg-conv crash
+# vss2svg-conv crash
 MAX=100
 
 burn_in_hell(){
     counter1=0
     while [ $counter1 -lt $MAX ]
     do
-        tmp_emf=`mktemp -p ../out/`
-        cp emf/`ls emf |shuf -n 1` ${tmp_emf} 
+        tmp_vss=`mktemp -p ../out/`
+        cp vss/`ls vss |shuf -n 1` ${tmp_vss} 
         counter2=0
         while [ $counter2 -lt $MAX ]
         do
-            ../../emf2svg-conv -p -w 800 -h 600 -i ${tmp_emf} -o ../out/test.svg -p
+            ../../vss2svg-conv -i ${tmp_vss} -o ../out/test/
             ret=$?
             if [ $ret -eq 1 ]
             then
@@ -29,14 +29,14 @@ burn_in_hell(){
             elif [ $ret -gt 1 ]
             then
                 ts=`date +"%F-%H%M%S"`
-                printf "[ERROR] corrupted file 'bad_corrupted_${ts}.emf' caused something wrong\n"
-                cp ${tmp_emf} ../out/bad_corrupted_${ts}.emf
+                printf "[ERROR] corrupted file 'bad_corrupted_${ts}.vss' caused something wrong\n"
+                cp ${tmp_vss} ../out/bad_corrupted_${ts}.vss
                 exit $ret
             fi
-            filesmasher-master/filesmasher ${tmp_emf} 1 >/dev/null
+            filesmasher-master/filesmasher ${tmp_vss} 1 >/dev/null
             counter2=$(( $counter2 + 1 ))
         done
-        rm "${tmp_emf}"
+        rm "${tmp_vss}"
         counter1=$(( $counter1 + 1 ))
     done
 }
